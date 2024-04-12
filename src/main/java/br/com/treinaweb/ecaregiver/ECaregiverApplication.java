7f259a3f335eb082e915e3d470fb6029cf252b53
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-04-11 22:11:14 -0300

    first commit

---
+++ src/main/java/br/com/treinaweb/ecaregiver/ECaregiverApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ecaregiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECaregiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECaregiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/ecaregiver/ECaregiverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ecaregiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECaregiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
